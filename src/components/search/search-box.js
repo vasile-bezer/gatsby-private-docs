88b108e169670c69dadb2229a6efa12a65e6dbba
Vasile Bezer <vasile.bezer.1@gmail.com>
2022-03-12 16:02:39 +0100

    search box changes

--- src/components/search/search-box.js
+++ src/components/search/search-box.js
@@ -1,19 +1,20 @@
 import React from "react"
 import { connectSearchBox } from "react-instantsearch-dom"
 import { Search as SearchIcon } from "@styled-icons/fa-solid"
+import MagnifyingGlass from "./magnifying-glass"
 export default connectSearchBox(
   ({ refine, currentRefinement, className, onFocus }) => (
     <form className={className}>
       <input
         className="SearchInput"
         type="text"
-        placeholder="Search"
+        placeholder="Cerca"
         aria-label="Search"
         onChange={e => refine(e.target.value)}
         value={currentRefinement}
         onFocus={onFocus}
       />
-      <SearchIcon className="SearchIcon" />
+      <MagnifyingGlass className="SearchIcon" />
     </form>
   )
 )
