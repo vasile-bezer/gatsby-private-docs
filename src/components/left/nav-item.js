49134689991532a925c53a216a20e26b4177e222
percy shelley <vasile.bezer.1@gmail.com>
2023-01-07 17:22:40 +0100

    pulizia

--- src/components/left/nav-item.js
+++ src/components/left/nav-item.js
@@ -52,16 +52,3 @@ const NavItem = ({ item }) => {
 };
 
 export default (NavItem);
-
-/*
-	per cambiare il peso dei font figlio si può usare
-	const NavItemChild = styled.ul`
-	margin: 0.5rem 0 0.5rem 0.5rem;
-	padding: 0;
-	list-style: none;
-	& > li > a {
-		margin: 0rem 0rem 0.3rem 0rem;
-		font-weight: normal;
-	}
-	`;
-*/
--- src/components/left/styled-nav-item-child.js
+++ src/components/left/styled-nav-item-child.js
@@ -5,3 +5,16 @@ margin: 0rem 0 0rem 0.5rem;
 padding: 0;
 list-style: none;
 `;
+
+/*
+	per cambiare il peso dei font figlio si può usare
+	const NavItemChild = styled.ul`
+	margin: 0.5rem 0 0.5rem 0.5rem;
+	padding: 0;
+	list-style: none;
+	& > li > a {
+		margin: 0rem 0rem 0.3rem 0rem;
+		font-weight: normal;
+	}
+	`;
+*/
