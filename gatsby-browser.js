4ccaf0a7eec6c696f0e0967182921c8a2121c602
percy shelley <vasile.bezer.1@gmail.com>
2023-02-02 23:32:18 +0100

    added shouldUpdateScroll to gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,6 +11,18 @@
 const React = require("react")
 const GlobalContextProvider =
   require("./src/context/GlobalContextProvider").default
+
+export const shouldUpdateScroll = ({
+  routerProps: {location},
+  getSavedScrollPosition
+})=>{
+  if(location.action === "PUSH"){
+    // window.setTimeout(() => window.scrollTo(0, 0), 0);
+  } else {
+    const savedPosition = getSavedScrollPosition(location);
+   //  window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), 0);
+  }
+}
 export const wrapRootElement = ({ element }) => (
   <GlobalContextProvider>{element}</GlobalContextProvider>
 )
