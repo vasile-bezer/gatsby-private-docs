396799462cce2fbdfd5d4259b6d4e0f97bd0a2ec
percy shelley <vasile.bezer.1@gmail.com>
2023-02-02 23:34:51 +0100

    import { navigate } from "@reach/router"

--- src/components/private-route.js
+++ src/components/private-route.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { navigate } from "gatsby"
+import { navigate } from "@reach/router"
 import { isLoggedIn, isBrowser } from "auth/auth"
 
 const PrivateRoute = ({ component: Component, location, body, ...rest }) => {
